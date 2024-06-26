8eebbe2ede676169d701da9a482b8b14c4cef408
eerieA <ninja.duan@gmail.com>
2024-06-25 23:07:24 -0700

    Trying out middleware chain and router matcher

--- api/src/lib/server.js
+++ api/src/lib/server.js
@@ -6,12 +6,32 @@ export const startServer = () => {
     const httpServer = express();
     const port = config.port;
 
-    httpServer.get('/ping', (req, res) => {
+    // .use for handling all incoming requests
+    httpServer.use((req, res, next) => {
+        console.log('1 - first express middleware func');
+        next();
+    })
+
+    // .use for handling any requests that stars with api
+    httpServer.use('/api', (req, res, next) => {
+        console.log('2 - second express middleware func');
+        next();
+    })
+
+    // TODO: abstract this to the router
+    httpServer.get('/api/ping', (req, res) => {
         console.log(`ℹ️ - Ping route: ${req.url} ${Date.now()}`);
         res.status(200).json({
             message: '✅ - {Pong: test successful'
         });
     })
+
+    httpServer.all('*', (req, res) => {
+        console.log(`4 - Catch all...`);
+        res.status(404).json({
+            message: `${req.url} not found`
+        })
+    });
 
     try {
         httpServer.listen(port, () => {
